1bb2bc699d2d23e26d4e405e6eb25727387bf420
Eren <nouman.laravel@outlook.com>
2026-01-21 15:47:55 +0800

    f

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: ["lh3.googleusercontent.com",
+    remotePatterns: ["lh3.googleusercontent.com",
     "lh4.googleusercontent.com",
     "lh5.googleusercontent.com",
     "lh6.googleusercontent.com",],
