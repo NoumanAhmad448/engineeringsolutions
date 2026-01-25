1dd0af8f4f2be4cd1b902137ecf7215db0587021
Eren <nouman.laravel@outlook.com>
2026-01-25 18:46:14 +0800

    run

--- resources/views/internships/apply.blade.php
+++ resources/views/internships/apply.blade.php
@@ -17,17 +17,17 @@
 
                                 <div class="form-group">
                                     <label>Name</label>
-                                    <input type="text" name="name" class="form-control" required>
+                                    <input type="text" name="name" class="form-control" required placeholder="Name">
                                 </div>
 
                                 <div class="form-group">
                                     <label>Email (optional)</label>
-                                    <input type="email" name="email" class="form-control">
+                                    <input type="email" name="email" class="form-control" placeholder="Email">
                                 </div>
 
                                 <div class="form-group">
                                     <label>Phone</label>
-                                    <input type="text" name="phone" class="form-control" required>
+                                    <input type="text" name="phone" class="form-control" required placeholder="Phone Number">
                                 </div>
 
                                 <div class="form-group">
