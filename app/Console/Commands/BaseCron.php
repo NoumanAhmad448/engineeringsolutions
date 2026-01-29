20940f2fa793677e8ddacfe8896258e57f29b0f9
Eren <nouman.laravel@outlook.com>
2026-01-29 19:41:31 +0800

    done

--- app/Console/Commands/BaseCron.php
+++ app/Console/Commands/BaseCron.php
@@ -24,26 +24,26 @@ abstract class BaseCron extends Command
             ]
         );
 
+        $this->cronJob->starts_at = LyskillsCarbon::now();
         try {
             // Execute the actual cron logic
             $erro_message = $this->runCron();
 
             // Success update
+            $this->updateCron($erro_message);
+        } catch (Exception $e) {
+            // Failure update
+            $this->updateCron($e->getMessage());
+            $this->error($e->getMessage());
+        }
+    }
+
+    private function updateCron($erro_message){
             $this->cronJob->update([
                 config('table.status')  => $erro_message ? config('constants.error') : config('constants.successed'),
                 config('table.message') => $erro_message ?? 'Cron executed successfully',
                 config('table.ends_at') => LyskillsCarbon::now(),
             ]);
-        } catch (Exception $e) {
-            // Failure update
-            $this->cronJob->update([
-                config('table.status')  => config('constants.error'),
-                config('table.message') => $e->getMessage(),
-                config('table.ends_at') => LyskillsCarbon::now(),
-            ]);
-
-            $this->error($e->getMessage());
-        }
     }
 
     // Child classes must implement this
